c67596a3bdf10992b2642fbd25814278021bda9e
Chris Pope <popechrisj@gmail.com>
2017-07-27 17:28:57 -0400

    Chris' first commit

--- tester/Test.java
+++ tester/Test.java
@@ -2,6 +2,6 @@ package tester;
 
 class Test{
     public static void main(String[] args){
-        System.out.println("Testing");
+        System.out.println("chakfgaaslkrgnaw something else");
     }
 }
